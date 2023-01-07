9e333b260a550321fd5fc0fd067ba053c0fc7b84
francois delattre <francoisdel@gmail.com>
2023-01-07 15:14:24 +0100

    il etait temps

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,44 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new # Needed to instantiate the form_with
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    # No need for app/views/restaurants/create.html.erb
+    redirect_to task_path(@task)
+  end
+
+  def update
+    @task.update(task_params_edit)
+    # No need for app/views/restaurants/update.html.erb
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+  
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
+  def task_params_edit
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+end
